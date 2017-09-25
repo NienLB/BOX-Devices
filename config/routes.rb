27d82f35225177868ce272a4418f5545a65ab172
Nien Luong <tatsuyanin@gmail.com>
2017-09-25 13:52:57 +0700

    Add Bootstrap 4.0.0.beta && add Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'status#index', as: 'status_index'
+
   resources :devices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
