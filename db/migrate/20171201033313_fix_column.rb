cd2481b28601a2cd5c5d52381f07d1761e26b1df
Nien Luong <tatsuyanin@gmail.com>
2017-12-08 16:00:43 +0700

    Delete db migration error

--- db/migrate/20171201033313_fix_column.rb
+++
@@ -1,9 +0,0 @@
-class FixColumn < ActiveRecord::Migration[5.0]
-  def self.up
-    rename_column :vendors, :strings  , :phone 
-  end
-
-  def self.down
-    # rename back if you need or do something else or do nothing
-  end  
-end
