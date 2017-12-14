49286741c9f5570997d2d616cc5fdc5c17613493
Nien Luong <tatsuyanin@gmail.com>
2017-12-14 16:05:21 +0700

    Fix nav
    Add link to logo

--- db/migrate/20171214080036_add_description_vendor.rb
+++ db/migrate/20171214080036_add_description_vendor.rb
@@ -1,5 +1,6 @@
 class AddDescriptionVendor < ActiveRecord::Migration[5.0]
   def change
-    add_column :vendors, :description, :string
+    #add_column :vendors, :description, :string
+    #add_column :vendors , :type_vendor, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214075045) do
+ActiveRecord::Schema.define(version: 20171214080036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20171214075045) do
     t.string   "name"
     t.string   "code"
     t.string   "description"
+    t.string   "type_vendor"
     t.index ["code"], name: "index_vendors_on_code", using: :btree
   end
 
