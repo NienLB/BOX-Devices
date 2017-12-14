696bd0342dc272e954fb149100b2fe93f609064f
Nien Luong <tatsuyanin@gmail.com>
2017-12-14 14:53:48 +0700

    Fix db vendor

---
+++ db/migrate/20171214075045_add_type_of_vendor.rb
@@ -0,0 +1,5 @@
+class AddTypeOfVendor < ActiveRecord::Migration[5.0]
+  def change
+    add_column :vendors , :type_vendor, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206073400) do
+ActiveRecord::Schema.define(version: 20171214075045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,6 @@ ActiveRecord::Schema.define(version: 20171206073400) do
     t.string   "name"
     t.string   "code"
     t.string   "description"
-    t.string   "type_vendor"
     t.index ["code"], name: "index_vendors_on_code", using: :btree
   end
 
